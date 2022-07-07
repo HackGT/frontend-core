0826d2b727497d6a68e10ccf4f9b7112b0a2f603
Ayush Goyal <agoyal2001@gmail.com>
2022-07-06 19:03:45 -0700

    Update imports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
 export { default as Footer } from "./components/footer/Footer";
-export { default as HexlabsLogo } from "./components/header/HexLabsLogo";
 export { default as Navbar } from "./components/header/Header";
-export { default as NavbarItem } from "./components/header/NavbarItem";

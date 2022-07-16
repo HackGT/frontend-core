d54e090bb15767e5d1923e78b76ebdd88b6c048f
Ayush Goyal <agoyal2001@gmail.com>
2022-07-16 10:10:14 -0700

    Fix duplicate import

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
 import { terser } from "rollup-plugin-terser";
 import external from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";

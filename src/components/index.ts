fd0072d366c4fe475530f49759ce9947a97f130d
rajbrady <raj@alan.app>
2022-07-21 10:26:16 -0700

    Updated with Loading

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Header } from "./header/Header";
 export { default as HeaderItem } from "./header/HeaderItem";
 export { default as ErrorScreen } from "./errorScreens/ErrorScreen";
 export { default as NotFoundScreen } from "./errorScreens/NotFoundScreen";
+export { default as Loading } from "./errorScreens/Loading";

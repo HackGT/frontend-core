d1320bcb54776d072fedb2f12b323b248c032db4
Ayush Goyal <agoyal2001@gmail.com>
2022-08-31 16:57:55 -0400

    Update loading screen

--- src/components/loadingScreen/LoadingScreen.tsx
+++ src/components/loadingScreen/LoadingScreen.tsx
@@ -2,7 +2,7 @@ import { Center, Spinner } from "@chakra-ui/react";
 import React from "react";
 
 const LoadingScreen: React.FC = () => (
-  <Center h="100vh">
+  <Center h="calc(100vh)">
     <Spinner size="xl" />
   </Center>
 );

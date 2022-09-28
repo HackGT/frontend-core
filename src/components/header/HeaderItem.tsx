1669e910a07e1b9b4171249ed8220f71509872b7
Ayush Goyal <agoyal2001@gmail.com>
2022-09-27 22:24:49 -0400

    Add headeritem other props

--- src/components/header/HeaderItem.tsx
+++ src/components/header/HeaderItem.tsx
@@ -1,12 +1,18 @@
 import React, { PropsWithChildren } from "react";
 import { Text } from "@chakra-ui/react";
 
-interface HeaderItemProps {
+interface HeaderItemProps extends React.ComponentProps<"p"> {
   show?: boolean;
 }
 
-const HeaderItem: React.FC<PropsWithChildren<HeaderItemProps>> = (props) => (
-  <Text style={{ lineHeight: "48px" }}>{props.children}</Text>
+const HeaderItem: React.FC<PropsWithChildren<HeaderItemProps>> = ({
+  show,
+  children,
+  ...otherProps
+}) => (
+  <Text lineHeight="48px" {...otherProps}>
+    {children}
+  </Text>
 );
 
 export default HeaderItem;
